c5a67518f579e20a89c9bb8e713420c8a8b0f98d
AhmarWorld <ahmarkakimov2005@gmail.com>
2023-10-16 21:58:42 +0600

    Complete add Redux

--- components/Recipes/Recipes.js
+++ components/Recipes/Recipes.js
@@ -2,62 +2,57 @@ import { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { getRecipesByCategory } from "../../lib/getRecipesByCategory";
 import Recipe from "../Recipe/Recipe";
-const Recipes = ({ activeCategory, inputText }) => {
-    const [recipes, setRecipes] = useState([]);
-    const [showingRecipes, setShowingRecipes] = useState([]);
+import { useSelector } from "react-redux";
+const Recipes = ({ inputText }) => {
+  const [showingRecipes, setShowingRecipes] = useState([]);
+  const activeCategory = useSelector(
+    (state) => state.categories.activeCategory
+  );
+  const recipes = useSelector((state) => state.recipes.recipes);
+  useEffect(() => {
+    setShowingRecipes(recipes);
+  }, [activeCategory]);
 
-    useEffect(() => {
-        (async () => {
-            if (activeCategory) {
-                const res = await getRecipesByCategory(activeCategory);
-                setRecipes(res.meals);
-                setShowingRecipes(res.meals);
-            }
-        })();
-    }, [activeCategory]);
+  useEffect(() => {
+    if (inputText.trim() !== "" && recipes.length > 0) {
+      const newShowingRecipes = recipes.filter((recipe) => {
+        return recipe.strMeal.toLowerCase().includes(inputText.toLowerCase());
+      });
+      setShowingRecipes(newShowingRecipes);
+    }
+  }, [inputText]);
 
-    useEffect(() => {
-        if (inputText.trim() !== "" && recipes.length > 0) {
-            const newShowingRecipes = recipes.filter((recipe) => {
-                return recipe.strMeal
-                    .toLowerCase()
-                    .includes(inputText.toLowerCase());
-            });
-            setShowingRecipes(newShowingRecipes);
-        }
-    }, [inputText]);
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Recipes</Text>
-            <FlatList
-                data={showingRecipes}
-                numColumns={2}
-                renderItem={({ item }) => {
-                    return <Recipe recipe={item} />;
-                }}
-                keyExtractor={(item) => item.idMeal}
-                style={styles.recipeList}
-                columnWrapperStyle={styles.recipeListColumnWrapper}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Recipes</Text>
+      <FlatList
+        data={showingRecipes}
+        numColumns={2}
+        renderItem={({ item }) => {
+          return <Recipe recipe={item} />;
+        }}
+        keyExtractor={(item) => item.idMeal}
+        style={styles.recipeList}
+        columnWrapperStyle={styles.recipeListColumnWrapper}
+      />
+    </View>
+  );
 };
 export default Recipes;
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 16,
-        gap: 20,
-    },
-    heading: {
-        fontWeight: "600",
-        fontSize: 20,
-    },
-    recipeList: {
-        gap: 18,
-    },
-    recipeListColumnWrapper: {
-        justifyContent: "space-between",
-        gap: 12,
-    },
+  container: {
+    marginTop: 16,
+    gap: 20,
+  },
+  heading: {
+    fontWeight: "600",
+    fontSize: 20,
+  },
+  recipeList: {
+    gap: 18,
+  },
+  recipeListColumnWrapper: {
+    justifyContent: "space-between",
+    gap: 12,
+  },
 });
--- store/categoriesSlice.js
+++ store/categoriesSlice.js
@@ -1,39 +1,39 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getCategories } from '../lib/getCategories';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getCategories } from "../lib/getCategories";
 
 const initialState = {
-    activeCategory: '',
-    categories: [],
-    loaded: false,
+  activeCategory: "",
+  categories: [],
+  loaded: false,
 };
 
 export const fetchCategories = createAsyncThunk(
-    'categories/fetchCategories',
-    async () => {
-        const res = await getCategories();
-        return res;
-    }
+  "categories/fetchCategories",
+  async () => {
+    const res = await getCategories();
+    return res;
+  }
 );
 
 export const categoriesSlice = createSlice({
-    name: 'categories',
-    initialState: { ...initialState },
-    reducers: {
-        updateActiveCategory(state, action) {
-            state.activeCategory = action.payload;
-        },
+  name: "recipesSlice",
+  initialState: { ...initialState },
+  reducers: {
+    updateActiveCategory(state, action) {
+      state.activeCategory = action.payload;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchCategories.pending, (state) => {
-                state.loaded = false;
-            })
-            .addCase(fetchCategories.fulfilled, (state, action) => {
-                state.categories = action.payload.categories;
-                state.activeCategory = action.payload.categories[0].strCategory;
-                state.loaded = true;
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.loaded = false;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.categories = action.payload.categories;
+        state.activeCategory = action.payload.categories[0].strCategory;
+        state.loaded = true;
+      });
+  },
 });
 
 export const { updateActiveCategory } = categoriesSlice.actions;
